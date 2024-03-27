101bd1a3741268e24194d11079bef230e455364c
Egorfdfa <rebrikov.2021@mail.ru>
2024-03-27 09:57:27 +0300

    ffswf

---
+++ backend/modules/blog/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Article, Category
+from mptt.admin import DraggableMPTTAdmin
+
+
+@admin.register(Category)
+class CategoryAdmin(DraggableMPTTAdmin):
+    list_display = ('tree_actions', 'indented_title', 'id', 'title', 'slug')
+    list_display_links = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+    fieldsets = (
+        ('Основная информация', {'fields': ('title', 'slug', 'parent')}),
+        ('Описание', {'fields': ('description',)})
+    )
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)} 
